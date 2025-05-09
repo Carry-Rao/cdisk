736a0b5dc29c947834a4f7c2bdb7baeeb87717ef
GitHub Carry-Rao <GitHub carry3658@outlook.com>
2025-05-09 23:32:07 +0800

    a

--- routes/routes.go
+++ routes/routes.go
@@ -3,31 +3,19 @@ package routes
 import (
 	"net/http"
 
-	"github.com/Carry-Rao/webdisk/handlers"
-	"github.com/Carry-Rao/webdisk/middlewares"
-	"github.com/Carry-Rao/webdisk/utils"
 	"github.com/gorilla/mux"
 )
 
-func NewRouter() *mux.Router {
-	r := mux.NewRouter()
+func InitRoutes() *mux.Router {
+	router := mux.NewRouter().StrictSlash(true)
 
-	r.Use(middlewares.LoggerMiddleware)
-	r.Use(middlewares.RecoveryMiddleware)
+	router.HandleFunc("/", IndexHandler).Methods("GET")
+	router.HandleFunc("/upload", UploadHandler).Methods("POST")
+	router.HandleFunc("/download/{file_name}", DownloadHandler).Methods("GET")
+	router.HandleFunc("/delete/{file_name}", DeleteHandler).Methods("GET")
 
-	r.HandleFunc("/api/v1/upload", handlers.UploadHandler).Methods(http.MethodPost)
-	r.HandleFunc("/api/v1/download", handlers.DownloadHandler).Methods(http.MethodGet)
-	r.HandleFunc("/api/v1/delete", handlers.DeleteHandler).Methods(http.MethodDelete)
-	r.HandleFunc("/api/v1/list", handlers.ListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/api/v1/info", handlers.InfoHandler).Methods(http.MethodGet)
-	r.HandleFunc("/api/v1/share", handlers.ShareHandler).Methods(http.MethodPost)
-	r.HandleFunc("/api/v1/unshare", handlers.UnshareHandler).Methods(http.MethodDelete)
-	r.HandleFunc("/api/v1/share/list", handlers.ShareListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/api/v1/share/info", handlers.ShareInfoHandler).Methods(http.MethodGet)
-	r.HandleFunc("/api/v1/share/download", handlers.ShareDownloadHandler).Methods(http.MethodGet)
-	r.HandleFunc("/api/v1/share/delete", handlers.ShareDeleteHandler).Methods(http.MethodDelete)
+	return router
+}
 
-	r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(utils.GetAbsPath("web/dist/")))))
-
-	return r
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
 }
