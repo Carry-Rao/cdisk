a1b9e8a12cee492c19c02410945df8b032ef67ef
GitHub Carry-Rao <GitHub carry3658@outlook.com>
2025-05-09 23:35:33 +0800

    commit

--- routes/routes.go
+++ routes/routes.go
@@ -3,16 +3,17 @@ package routes
 import (
 	"net/http"
 
+	"github.com/Carry-Rao/webdisk/handlers"
 	"github.com/gorilla/mux"
 )
 
 func InitRoutes() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/", IndexHandler).Methods("GET")
-	router.HandleFunc("/upload", UploadHandler).Methods("POST")
-	router.HandleFunc("/download/{file_name}", DownloadHandler).Methods("GET")
-	router.HandleFunc("/delete/{file_name}", DeleteHandler).Methods("GET")
+	router.HandleFunc("/", handlers.IndexHandler).Methods("GET")
+	router.HandleFunc("/upload", handlers.UploadHandler).Methods("POST")
+	router.HandleFunc("/download/{file_name}", handlers.DownloadHandler).Methods("GET")
+	router.HandleFunc("/delete/{file_name}", handlers.DeleteHandler).Methods("GET")
 
 	return router
 }
